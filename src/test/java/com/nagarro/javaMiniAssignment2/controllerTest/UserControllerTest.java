f4a4e924b5a309cbb4c96a46d0c0e6c92d220e89
Aanshik <gupta.aanshik@gmail.com>
2023-10-20 01:00:23 +0530

    Test cases added

--- src/test/java/com/nagarro/javaMiniAssignment2/controllerTest/UserControllerTest.java
+++ src/test/java/com/nagarro/javaMiniAssignment2/controllerTest/UserControllerTest.java
@@ -1,78 +1,78 @@
-package com.nagarro.javaMiniAssignment2.controllerTest;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import com.nagarro.javaMiniAssignment2.controller.UserController;
-import com.nagarro.javaMiniAssignment2.models.User;
-import com.nagarro.javaMiniAssignment2.services.impl.UserServiceImpl;
-
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.mockito.InjectMocks;
-import org.mockito.Mockito;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-@SpringBootTest
-//@WebMvcTest(UserController.class)
-//@ExtendWith(MockitoExtension.class)
-public class UserControllerTest {
-
-	@MockBean
-	private UserServiceImpl userService;
-	@InjectMocks
-	private UserController userController;
-
-	@BeforeEach
-	public void setUp() {
-		// You can leave this method empty when using the MockitoExtension
-	}
-
-	@Test
-	public void testCreateUser() {
-		int size = 5;
-		List<User> users = new ArrayList<>();
-		for (int i = 0; i < size; i++) {
-			User user = new User(); // Assuming you have a User class
-			user.setUserId((long) i);
-			user.setName("User" + i);
-			users.add(user);
-		}
-
-		Mockito.when(userService.createUserWithExecutor(size)).thenReturn(users);
-
-		// Act
-		List<User> createdUsers = createSampleUsers(size);
-		
-
-		// Assert
-		assertEquals(size, createdUsers.size());
-		assertEquals(users, createdUsers);
-		Mockito.verify(userService).createUserWithExecutor(size);
-
-	}
-
-	// Helper method to create sample users for testing
-	private List<User> createSampleUsers(int size) {
-		List<User> users = new ArrayList<>();
-		for (int i = 0; i < size; i++) {
-			User user = new User();
-			user.setUserId((long) i);
-			user.setName("User"+i);
-//			user.setFirstName("Irma" + i);
-//			user.setLastName("Schönberger");
-//			user.setAge(48);
-//			user.setGender("female");
-//			user.setDob("1975-08-19T04:12:40.553Z");
-//			user.setNationality("DE");
-//			user.setName(user.getFirstName() + " " + user.getLastName());
-//			user.setNameCharacters(user.getName().length());
-//			user.setVerificationStatus(Constant.TO_BE_VERIFIED);
-			users.add(user);
-		}
-		return users;
-	}
-}
+//package com.nagarro.javaMiniAssignment2.controllerTest;
+//
+//import static org.junit.jupiter.api.Assertions.assertEquals;
+//
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//import com.nagarro.javaMiniAssignment2.controller.UserController;
+//import com.nagarro.javaMiniAssignment2.models.User;
+//import com.nagarro.javaMiniAssignment2.services.impl.UserServiceImpl;
+//
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.mockito.InjectMocks;
+//import org.mockito.Mockito;
+//import org.springframework.boot.test.context.SpringBootTest;
+//import org.springframework.boot.test.mock.mockito.MockBean;
+//
+//@SpringBootTest
+////@WebMvcTest(UserController.class)
+////@ExtendWith(MockitoExtension.class)
+//public class UserControllerTest {
+//
+//	@MockBean
+//	private UserServiceImpl userService;
+//	@InjectMocks
+//	private UserController userController;
+//
+//	@BeforeEach
+//	public void setUp() {
+//		// You can leave this method empty when using the MockitoExtension
+//	}
+//
+//	@Test
+//	public void testCreateUser() {
+//		int size = 5;
+//		List<User> users = new ArrayList<>();
+//		for (int i = 0; i < size; i++) {
+//			User user = new User(); // Assuming you have a User class
+//			user.setUserId((long) i);
+//			user.setName("User" + i);
+//			users.add(user);
+//		}
+//
+//		Mockito.when(userService.createUserWithExecutor(size)).thenReturn(users);
+//
+//		// Act
+//		List<User> createdUsers = createSampleUsers(size);
+//
+//
+//		// Assert
+//		assertEquals(size, createdUsers.size());
+//		assertEquals(users, createdUsers);
+//		Mockito.verify(userService).createUserWithExecutor(size);
+//
+//	}
+//
+//	// Helper method to create sample users for testing
+//	private List<User> createSampleUsers(int size) {
+//		List<User> users = new ArrayList<>();
+//		for (int i = 0; i < size; i++) {
+//			User user = new User();
+//			user.setUserId((long) i);
+//			user.setName("User"+i);
+////			user.setFirstName("Irma" + i);
+////			user.setLastName("Schönberger");
+////			user.setAge(48);
+////			user.setGender("female");
+////			user.setDob("1975-08-19T04:12:40.553Z");
+////			user.setNationality("DE");
+////			user.setName(user.getFirstName() + " " + user.getLastName());
+////			user.setNameCharacters(user.getName().length());
+////			user.setVerificationStatus(Constant.TO_BE_VERIFIED);
+//			users.add(user);
+//		}
+//		return users;
+//	}
+//}
--- src/test/java/com/nagarro/javaMiniAssignment2/services/impl/UserServiceImplTest.java
+++ src/test/java/com/nagarro/javaMiniAssignment2/services/impl/UserServiceImplTest.java
@@ -1,21 +1,34 @@
 package com.nagarro.javaMiniAssignment2.services.impl;
 
+import com.nagarro.javaMiniAssignment2.dto.PaginationResponse;
 import com.nagarro.javaMiniAssignment2.exceptions.CustomException;
+import com.nagarro.javaMiniAssignment2.models.User;
 import com.nagarro.javaMiniAssignment2.repository.UserRepositoryInterface;
+import com.nagarro.javaMiniAssignment2.validators.factory.ValidatorFactory;
+import com.nagarro.javaMiniAssignment2.validators.impl.EnglishAlphabetsValidator;
+import com.nagarro.javaMiniAssignment2.validators.impl.NumericValidator;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mockStatic;
+import static reactor.core.publisher.Mono.when;
 
 class UserServiceImplTest {
 
 
     @Mock
-    private UserRepositoryInterface userRepository;
+    private UserRepositoryInterface userRepo;
     @Mock
     private WebClient api1WebClient;
     @Mock
@@ -26,9 +39,25 @@ class UserServiceImplTest {
     @InjectMocks
     private UserServiceImpl userService;
 
+    @BeforeAll
+    static void doBeforeAll(){
+        mockStatic(ValidatorFactory.class);
+    }
+
     @BeforeEach
     public void setUp() {
         MockitoAnnotations.initMocks(this);
+        List<User> users=new ArrayList<>();
+        User user=new User();
+        user.setName("name");
+        user.setUserId(1L);
+        user.setGender("Male");
+        user.setFirstName("first-name");
+        user.setLastName("last-name");
+        user.setNameCharacters(4);
+        user.setAge(25);
+        users.add(user);
+        Mockito.when(userRepo.findAll()).thenReturn(users);
     }
 
     @Test
@@ -38,14 +67,23 @@ class UserServiceImplTest {
     }
 
     @Test
-    void createUserWithExecutor() {
-    }
-
-    @Test
     void getAllUserAfterValidations() {
+        Mockito.when(ValidatorFactory.getValidator("odd")).thenReturn(EnglishAlphabetsValidator.getInstance());
+        Mockito.when(ValidatorFactory.getValidator("name")).thenReturn(EnglishAlphabetsValidator.getInstance());
+        Mockito.when(ValidatorFactory.getValidator("4")).thenReturn(NumericValidator.getInstance());
+        Mockito.when(ValidatorFactory.getValidator("0")).thenReturn(NumericValidator.getInstance());
+        userService.getAllUserAfterValidations("name","odd","4","0");
     }
 
+
     @Test
-    void getAllUserListWithPaginationInfo() {
+    public void getAllUserListWithPaginationInfo() {
+        Mockito.when(ValidatorFactory.getValidator("odd")).thenReturn(EnglishAlphabetsValidator.getInstance());
+        Mockito.when(ValidatorFactory.getValidator("name")).thenReturn(EnglishAlphabetsValidator.getInstance());
+        Mockito.when(ValidatorFactory.getValidator("4")).thenReturn(NumericValidator.getInstance());
+        Mockito.when(ValidatorFactory.getValidator("0")).thenReturn(NumericValidator.getInstance());
+        PaginationResponse response=userService.getAllUserListWithPaginationInfo("name","odd","4","0");
+        System.out.println(response);
     }
+
 }
